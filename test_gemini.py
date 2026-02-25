d84c277be88982e1ffeeb3d514c666a0071bfd9c
tefjuu <t.rechtorovicova@seznam.cz>
2026-02-25 18:09:55 +0100

    Update test_gemini.py

--- test_gemini.py
+++ test_gemini.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Gemini test", layout="centered")
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 st.title("Test Gemini napojenia")
 
